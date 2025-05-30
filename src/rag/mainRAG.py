34b44a82f5f641b40751b944a4831dc5244b5254
Igor Asmykovich <iasmykovich@epo.org>
2025-05-30 18:56:59 +0200

    streaming works now: 1. call the model with tools and no streaming to get the tools and run the search, 2. call the model with straming and no tools as the tools have already been called.

--- src/rag/mainRAG.py
+++ src/rag/mainRAG.py
@@ -10,11 +10,13 @@ from rag.RAGConfig import RAGConfig
     cfg_dict = yaml.safe_load(f) """
 
 logging.basicConfig(
-    level=logging.INFO,
+    level=logging.DEBUG,
     # format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     format="%(name)s: %(message)s",
 )
 logger = logging.getLogger(__name__)
+
+logger.setLevel(logging.DEBUG)
 
 
 # Environment variables. Requires PINECONE_KEY (free tier)
@@ -24,18 +26,6 @@ PINECONE_KEY = os.getenv("PINECONE_KEY")
 
 logger.debug(PINECONE_KEY)
 
-
-# --- Config ---
-MODEL_ID = RAGConfig.model_id
-
-MODEL_REGION = RAGConfig.model_aws_region
-MODEL_RUNTIME = RAGConfig.model_runtime
-
-INDEX_NAME = RAGConfig.index_name
-EMBEDDING_DIM = RAGConfig.embedding_dim
-METRIC = RAGConfig.metric
-CLOUD = RAGConfig.cloud
-PINECONE_REGION = RAGConfig.pinecone_region
 
 model_prompt = "Explain the theory of relativity in simple terms."
 SEARCH_QUESTION = "What is the key advantages of LoRA?"
@@ -60,6 +50,25 @@ if __name__ == "__main__":
             }
         ],
         "toolChoice": {"auto": {}},
+    }
+    
+    tool_config_no_tool = {
+        "tools": [
+            {
+                "toolSpec": {
+                    "name": "search_document",
+                    "description": "Search through the uploaded document for relevant passages",
+                    "inputSchema": {
+                        "json": {
+                            "type": "object",
+                            "properties": {"query": {"type": "string"}},
+                            "required": ["query"],
+                        }
+                    },
+                }
+            }
+        ],
+        "toolChoice": {"none": {}},
     }
 
     prompt = f"Q: {SEARCH_QUESTION}\n\n"
